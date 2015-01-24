51c34fe5b94ee2c1977b7ee64ca762c8f7d42494
Syohei YOSHIDA <syohex@gmail.com>
2015-01-25 00:06:34 +0900

    remove duplicate against all sources

--- auto-complete.el
+++ auto-complete.el
@@ -1078,27 +1078,29 @@ You can not use it in source definition like (prefix . `NAME')."
         for source in ac-current-sources
         ;; Delete duplicates from the same source, but allow multiple sources to
         ;; provide the same candidate completion.
-        append (delete-dups (ac-candidates-1 source)) into candidates
+        append (ac-candidates-1 source) into candidates
         finally return
-        (if (and ac-use-comphist ac-comphist)
-            (if ac-show-menu
-                (let* ((pair (ac-comphist-sort ac-comphist candidates prefix-len ac-comphist-threshold))
-                       (n (car pair))
-                       (result (cdr pair))
-                       (cons (if (> n 0) (nthcdr (1- n) result)))
-                       (cdr (cdr cons)))
-                  (if cons (setcdr cons nil))
-                  (setq ac-common-part (try-completion ac-prefix result))
-                  (setq ac-whole-common-part (try-completion ac-prefix candidates))
-                  (if cons (setcdr cons cdr))
-                  result)
-              (setq candidates (ac-comphist-sort ac-comphist candidates prefix-len))
-              (setq ac-common-part (if candidates (popup-x-to-string (car candidates))))
-              (setq ac-whole-common-part (try-completion ac-prefix candidates))
-              candidates)
-          (setq ac-common-part (try-completion ac-prefix candidates))
-          (setq ac-whole-common-part ac-common-part)
-          candidates)))
+        (progn
+          (delete-dups candidates)
+          (if (and ac-use-comphist ac-comphist)
+              (if ac-show-menu
+                  (let* ((pair (ac-comphist-sort ac-comphist candidates prefix-len ac-comphist-threshold))
+                         (n (car pair))
+                         (result (cdr pair))
+                         (cons (if (> n 0) (nthcdr (1- n) result)))
+                         (cdr (cdr cons)))
+                    (if cons (setcdr cons nil))
+                    (setq ac-common-part (try-completion ac-prefix result))
+                    (setq ac-whole-common-part (try-completion ac-prefix candidates))
+                    (if cons (setcdr cons cdr))
+                    result)
+                (setq candidates (ac-comphist-sort ac-comphist candidates prefix-len))
+                (setq ac-common-part (if candidates (popup-x-to-string (car candidates))))
+                (setq ac-whole-common-part (try-completion ac-prefix candidates))
+                candidates)
+            (setq ac-common-part (try-completion ac-prefix candidates))
+            (setq ac-whole-common-part ac-common-part)
+            candidates))))
 
 (defun ac-update-candidates (cursor scroll-top)
   "Update candidates of menu to `ac-candidates' and redraw it."
@@ -1195,7 +1197,7 @@ string."
                       remaindar))
         end-of-symbol
       (point))))
- 
+
 (defun ac-expand-string (string &optional remove-undo-boundary)
   "Expand `STRING' into the buffer and update `ac-prefix' to `STRING'.
 This function records deletion and insertion sequences by `undo-boundary'.
